9848933a3eab0322bcdbf2fbd5a85525e59870eb
Akinloye3264 <e.akinloye@alustudent.com>
2025-02-19 09:18:00 -0800

    print float

--- python-hello_world/4-print_float.py
+++ python-hello_world/4-print_float.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-#This script Completes the source code in order to print the float stored in the variable number with a precision of 2 digits.
+
 
 number = 3.14159
 print(f"Float: {number:.2f}")
