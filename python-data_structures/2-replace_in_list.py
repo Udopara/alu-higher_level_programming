e1833c260807929c77c1e12e48c8a2e2ddb3a75f
Peter Udo Opara <p.opara@alustudent.com>
2025-03-05 05:54:23 -0800

    rep

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 # 2-replace_in_list.py
 
@@ -8,4 +7,3 @@ def replace_in_list(my_list, idx, element):
     if idx >= 0 and idx < len(my_list):
         my_list[idx] = element
     return (my_list)
-
