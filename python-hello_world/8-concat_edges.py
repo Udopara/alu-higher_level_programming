1cdc239bd219e732cb961cc49e6839bd50b30c89
Peter Udo Opara <p.opara@alustudent.com>
2025-03-05 03:11:19 -0800

    conc2

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\n language that combines remarkable power with very clear syntax"
-str = str[str.index("object"): str.index("\n") ] + " " + str[str.index("with"): str.index(" very") ] + " " + str[str.index("Python"): str.index(" is") ]
+str = "Python is an interpreted, interactive, object-oriented programming\
+         language that combines remarkable power with very clear syntax"
+str = (str[39:66] + str[-23:-17] + str[0:6])
 print(str)
